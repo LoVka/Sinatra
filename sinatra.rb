155e02e1185b9959c5403b375fdda0ebb02eb4ef
Lovka <lovkavo@gmail.com>
2014-11-10 22:04:00 +0200

    Sinatra+mongo

--- sinatra.rb
+++ sinatra.rb
@@ -1,13 +1,34 @@
 require 'sinatra'
+require 'mongoid'
+
+configure do
+  Mongoid.load!("./config/mongoid.yml")
+end
+
+class Info
+  include Mongoid::Document
+  field :memory, type: String
+end
+
+get '/' do
+  erb :index
+end
 
 get '/free' do
   headers['Content-Type'] = 'text/plain; charset=utf-8'
-  @command_r = `free -m`
+  @memory = `free -m`
+  Info.create(memory: @memory)
   erb :free
 end
 
 get '/df' do
   headers['Content-Type'] = 'text/plain; charset=utf-8'
-  @command_m = `df -h`
+  @processes = `df -h`
   erb :df
+end
+
+get '/stat' do
+  headers['Content-Type'] = 'text/plain; charset=utf-8'
+  @stat = Info.all.pluck(:memory)
+  erb :stat
 end
